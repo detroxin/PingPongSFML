bf96284f98c73377bbd9cafb91b784cb933b3999
detroxin <rus.solovey.09@mail.ru>
2025-12-29 19:10:24 +0300

    Add ScoreText

--- main.cpp
+++ main.cpp
@@ -1,30 +1,60 @@
 #include <SFML/Graphics.hpp>
-
-using namespace sf;
+#include <iostream>
 
 int main() {
-    RenderWindow window(VideoMode({800, 600}), L"PingPong", Style::Default);
+    sf::RenderWindow window(sf::VideoMode({800, 600}), L"PingPong", sf::Style::Default);
 
     window.setVerticalSyncEnabled(true);
 
-    CircleShape shape(100.f, 3);
+    sf::Font bitFont("bit5x3.ttf");
+
+    sf::Text scoreText(bitFont);
+
+    scoreText.setString("0 | 0");
+    scoreText.setCharacterSize(24);
+    scoreText.setFillColor(sf::Color::White);
+    scoreText.setStyle(sf::Text::Bold);
+    scoreText.setPosition({400, 0});
+
+    sf::FloatRect textRect = scoreText.getLocalBounds();
+    scoreText.setOrigin(textRect.getCenter());
+    scoreText.setPosition({800 / 2, 25});
+
+    sf::CircleShape shape(75.f, 6);
     shape.setOrigin(shape.getGeometricCenter());
     shape.setPosition({400, 300});
-    shape.setFillColor(Color::White);
+    shape.setFillColor(sf::Color::White);
 
-    while(window.isOpen()) {
-        while(auto event = window.pollEvent()) {
-            if(event->is<sf::Event::Closed>()) {
+    sf::Clock clock;
+
+    while(window.isOpen()) 
+    {
+        while(auto event = window.pollEvent()) 
+        {
+            if(event->is<sf::Event::Closed>()) 
+            {
                 window.close();
-            } else if(auto keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+            } 
+            else if(auto keyPressed = event->getIf<sf::Event::KeyPressed>()) 
+            {
                 if(keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                {
                     window.close();
+                }
             }
         }
 
-        window.clear(Color::Black);
+        
+
+        sf::Time dt = clock.restart();
+
+        window.clear(sf::Color::Black);
+
         window.draw(shape);
+        window.draw(scoreText);
+
         window.display();
-        shape.rotate(degrees(1.0f));
+
+        shape.rotate(sf::degrees(1.0f));
     }
 }
